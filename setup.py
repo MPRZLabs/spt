934fad654c58cc432af7414a16a170f1cad96a20
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-04-17 20:15:52 +0200

    made a curses-based app \o/

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='mprisfy',
       version='1.0',
       py_modules=['mpris','spotify'],
-      scripts=['spotify.py'],
+      scripts=['spotify.py','sptsh'],
       )
