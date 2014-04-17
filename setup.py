dce5d5d28a133665aa3714d5fbfa6688d3bfa2fb
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-04-17 20:58:00 +0200

    added some cool details to sptsh and bumped up the version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='mprisfy',
-      version='1.0',
+      version='1.2',
       py_modules=['mpris','spotify'],
       scripts=['spotify.py','sptsh'],
       )
