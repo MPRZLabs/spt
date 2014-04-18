949c268500e709e0fe2102aac37cc92fc4c1c626
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-04-18 21:05:40 +0200

    now comes the time for fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='mprisfy',
-      version='1.4',
+      version='1.5',
       py_modules=['mpris','spotify'],
       scripts=['spotify.py','sptsh'],
       )
